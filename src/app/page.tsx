d803d9ca70bbdbe70e749b64e670522102022418
KunjYadav <kunj.yadav555@gmail.com>
2025-03-01 21:23:43 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton mode="modal">
+    <button className="bg-red-500">Sign in</button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+}
