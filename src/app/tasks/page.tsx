5e0cd74e00a8ca6840c45f9777bffae399631e3a
KunjYadav <kunj.yadav555@gmail.com>
2025-03-04 01:17:37 +0530

    ok done

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,7 +1,7 @@
-export const revalidate = 60;
-
 async function TasksPage() {
-  const response = await fetch('https://socially-app-psi.vercel.app/api/tasks');
+  const response = await fetch('https://socially-app-psi.vercel.app/api/tasks', {
+    cache: 'no-store',
+  });
   const tasks = await response.json();
 
   console.log('tasks:', tasks);
