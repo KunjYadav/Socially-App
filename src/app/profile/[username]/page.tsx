cb671d4a2374b47ad67223d9d0c4bed1390804a4
KunjYadav <kunj.yadav555@gmail.com>
2025-03-04 00:28:23 +0530

    letest nexj.js dynamic routes error is fix

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -10,9 +10,9 @@ import ProfilePageClient from './ProfilePageClient';
 export async function generateMetadata({
   params,
 }: {
-  params: { username: string };
+  params: Promise<{ username: string }>;
 }) {
-  const user = await getProfileByUsername(params.username);
+  const user = await getProfileByUsername((await params).username);
   if (!user) return;
 
   return {
@@ -21,8 +21,8 @@ export async function generateMetadata({
   };
 }
 
-async function ProfilePageServer({ params }: { params: { username: string } }) {
-  const user = await getProfileByUsername(params.username);
+async function ProfilePageServer({ params }: { params: Promise<{ username: string }> }) {
+  const user = await getProfileByUsername((await params).username);
 
   if (!user) notFound();
 
