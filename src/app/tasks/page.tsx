4b634339b2847dfc8495bd365f0adf4e2fc17398
KunjYadav <kunj.yadav555@gmail.com>
2025-03-03 23:44:59 +0530

    profile is added

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,11 @@
+async function TasksPage() {
+  const response = await fetch('https://socially-app-psi.vercel.app/api/tasks', {
+    cache: 'no-store',
+  });
+  const tasks = await response.json();
+
+  console.log('tasks:', tasks);
+
+  return <div>TasksPage</div>;
+}
+export default TasksPage;
